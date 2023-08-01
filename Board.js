5cc47a2c48f55a31e77f4aff353e43b7d08d048a
gunjan130315 <gunjan13031516@users.noreply.github.com>
2023-08-01 14:59:00 +0530

    second commit

--- Board.js
+++ Board.js
@@ -1,43 +1,104 @@
-const Cell = require("./Cell");
+const Cell =require("./Cell.js"); 
+ 
+class Board { 
+  constructor() { 
+    this.cells = [ 
+      new Cell(), new Cell(), new Cell(), 
+      new Cell(), new Cell(), new Cell(), 
+      new Cell(), new Cell(), new Cell() 
+    ]; 
+  } 
+  isEmpty(cellNumber) { 
+    return this.cells[cellNumber].isEmpty(); 
+  } 
+ 
+  printBoard() { 
+    console.log(this.cells);
+    
+  } 
+ 
+  analyse() { 
+    //rows
+      if(
+        this.cells[0].mark === this.cells[1].mark &&
+        this.cells[2].mark === this.cells[1].mark &&
+        !this.cells[0].isEmpty()
+      )
+      {
+        return [this.cells[0].mark, "win"];
+      }
+      if(
+        this.cells[3].mark === this.cells[4].mark &&
+        this.cells[4].mark === this.cells[5].mark &&
+        !this.cells[3].isEmpty()
+      )
+      {
+        return [this.cells[3].mark, "win"];
+      }
+      if(
+        this.cells[6].mark === this.cells[7].mark &&
+        this.cells[7].mark === this.cells[8].mark &&
+        !this.cells[6].isEmpty()
+      )
+      {
+        return [this.cells[6].mark, "win"];
+      }
 
-class Board {
-    constructor() {
-        this.cells = [new Cell(),
-            new Cell(),
-            new Cell(),
-            new Cell(),
-            new Cell(),
-            new Cell(),
-            new Cell(),
-            new Cell(),
-            new Cell(),
-        ]
-    }
-    isEmpty(cellNumber) {
-        return this.cells[cellNumber].isEmpty()
-    }
-    printBoard() {
-        console.log(this.cells)
-    }
-
-    analyse() {
-
-        const winPatterns = [
-         [0,1,2],[3,4,5],[6,7,8],
-         [0,3,6],[1,4,7],[2,5,8],
-         [0,4,8],[2,4,6]
-         ];
-
-         for (const pattern of winPatterns) {
-            const[a,b,c] = pattern ;
-            if (this.cells[a] === this.cells[b] && this.cells[a] == "Z") {
-                return [this.cells[a].mark,"Win"];
-            }
-            if (this.cells.every(cell => cell.mark !== "Z"))
-            return ["Draw"]
-         }
-
-        return["" ,""]
-    }
+      //columns
+      if(
+        this.cells[0].mark === this.cells[3].mark &&
+        this.cells[3].mark === this.cells[6].mark &&
+        !this.cells[0].isEmpty()
+      )
+      {
+        return [this.cells[0].mark, "win"];
+      }
+      if(
+        this.cells[1].mark === this.cells[4].mark &&
+        this.cells[4].mark === this.cells[7].mark &&
+        !this.cells[1].isEmpty()
+      )
+      {
+        return [this.cells[1].mark, "win"];
+      }
+      if(
+        this.cells[2].mark === this.cells[5].mark &&
+        this.cells[5].mark === this.cells[8].mark &&
+        !this.cells[2].isEmpty()
+      )
+      {
+        return [this.cells[2].mark, "win"];
+      }
+ //Diagonals
+      
+ if(
+  this.cells[0].mark === this.cells[4].mark &&
+  this.cells[4].mark === this.cells[8].mark &&
+  !this.cells[0].isEmpty()
+)
+{
+  return [this.cells[0].mark, "win"];
 }
-module.exports = Board
+if(
+  this.cells[2].mark === this.cells[4].mark &&
+  this.cells[4].mark === this.cells[6].mark &&
+  !this.cells[2].isEmpty()
+)
+{
+  return [this.cells[2].mark, "win"];
+}
+
+    
+if (this.cells.every(cell => cell.mark !== "Z")) { 
+     
+      return ["", "Draw"]; 
+
+    } 
+
+ 
+    return ["", ""]; 
+  } 
+} 
+
+ 
+module.exports=Board;
--- Game.js
+++ Game.js
@@ -1,51 +1,74 @@
- const Board = require("./Board");
- const Player = require("./Player");
- 
- class Game {
-    constructor (board,player0,player1) {
-        this.board = board ;
-        this.player0 = player0 ;
-        this.player1 = player1 ;
-        this.turn = 0 ;
-        this.isGameOver = false ;
-    }
-    static newGame(player0Name,player1Name) {
-        //validation 
-        let board = new Board()
-        let player0 = new Player(player0Name, "X")
-        let player1 = new Player(player1Name, "O")
-        return new Game(board,player0,player1)
-    }
+const Cell = require("./Cell");
+const Board = require("./Board");
+const Player = require("./Player");
 
-    play(cellNumber){
-        //validate CellNumber
-        if (this.isGameOver){
-            return false 
-        }
-       let isCellEmpty = this.board.isEmpty(cellNumber)
-       if(isCellEmpty){
-        return "Cell Already Marked"
-       }
-       if (this.turn % 2 == 0) {
-        currentPlayer = this.player0
-        } else {
-            currentPlayer = this.player1
-        }
-        currentPlayer.markCell(this.board.cells[cellNumber]) ;
-        this.turn++
-         let [winnerSymbol , gameState] =this.board.analyse()
-         if(gameState == ""){
-            return "Continue"
-        }
-        if (gameState == "Win !"){
-            if(winnerSymbol=="X") {
-                return this.player0.name + "is Winner"
-            }  return this.player1.name + "is Winner"
-        }
-        this.isGameOver = true
-        return "Game has been Drawed"
-    }
-
-}
-const g1 = Game.newGame("Gunjan","Neel")
-g1.play();
+class Game {
+    constructor(board, player0, player1) { 
+      this.board = board; 
+      this.player0 = player0; 
+      this.player1  = player1 ; 
+      this.turn = 0; 
+      this.isGameOver = false; 
+    } 
+   
+    static newGame(player0Name, player1Name) { 
+      let board = new Board(); 
+      let player0 = new Player(player0Name, "X"); 
+      let player1 = new Player(player1Name, "O"); 
+   
+      return new Game(board, player0 , player1); 
+    } 
+   
+    play(cellNumber) { 
+      if (this.isGameOver) { 
+        return "Game has ended, Please start the new game"; 
+      } 
+     
+      
+      if(!Number.isInteger(cellNumber) || cellNumber < 0 || cellNumber > 8) { 
+        return "Invalid number"; 
+      } 
+     
+      let isCellEmpty = this.board.isEmpty(cellNumber); 
+      let currentPlayer; 
+      if (!isCellEmpty) { 
+        return "Cell already Marked"; 
+      } 
+      if (this.turn % 2 === 0) { 
+        currentPlayer = this.player1; 
+      } else { 
+        currentPlayer = this.player2; 
+      } 
+      currentPlayer.markCell(this.board.cells[cellNumber]); 
+      this.turn++; 
+      this.board.printBoard(); 
+      let [winnerSymbol, gameState] = this.board.analyse(currentPlayer.symbol);    
+      
+      if (gameState == "win") { 
+        this.isGameOver = true; 
+        if(winnerSymbol == "X"){
+          return this.player0.name+" is winner"
+        } 
+        return this.player1.name+" is winner"
+      } 
+       if (gameState === "Draw") { 
+        this.isGameOver = true; 
+        return "Game has been Drawn"; 
+      } 
+      return "continue"; 
+    }   
+  }
+  
+let g1 = Game.newGame("Gunjan","Neel"); 
+// g1.play();
+          
+console.log(g1.play(0)); 
+console.log(g1.play(1)); 
+console.log(g1.play(2)); 
+console.log(g1.play(3)); 
+console.log(g1.play(4)); 
+console.log(g1.play(5));
+console.log(g1.play(6));
+console.log(g1.play(7));
+  
+  
